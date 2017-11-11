01e07951273fc9ec14e0220b34d36c85e0a67b49
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-11 15:53:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  const newdriver = Object.assign({}, driver);
+  let newdriver = Object.assign({}, driver);
   delete newdriver.key;
 }
 
